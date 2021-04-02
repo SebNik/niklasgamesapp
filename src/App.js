1d16c9b0be882e66be878b3fb277a2f92af6fc10
SebNik <lord.voldmeort1492@gmail.com>
2021-04-02 18:24:22 +0200

    added new path and new router libary

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import Game from './components/Game'
 import './App.css';
+import {BrowserRouter} from "react-router-dom";
+import Route from "react-router-dom/Route"
 
 function App() {
     return (
-        <div className="layout">
-            <Game />
-        </div>
+        <BrowserRouter>
+            <div className="layout">
+                <Route  path="/" render={
+                    () => {
+                        return <h1>Niklas Test</h1>
+                    }
+                }/>
+
+                <Route  path="/" render={
+                    () => {
+                        return <h1>Niklas Test</h1>
+                    }
+                }/>
+
+            </div>
+        </BrowserRouter>
     );
 }
 
