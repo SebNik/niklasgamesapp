f991ab9f32a6f84c48e11d11fcbfe08cd29a701f
SebNik <lord.voldmeort1492@gmail.com>
2021-04-06 22:25:09 +0200

    back to start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React from 'react';
-import TicTacToeGame from './games/tic_tac_toe/Game'
-import About from "./pages/About";
 import './App.css';
-import {BrowserRouter, NavLink} from "react-router-dom";
-import Route from "react-router-dom/Route"
 
 function App() {
     return (
-        <BrowserRouter>
-            <div className="layout">
+        <div className="App">
 
-                <Route  path="/games" exact ={true} component={About}/>
-
-                <Route  path="/about" exact ={true} component={About}/>
-                {/*<NavLink strict exact to="/about">About</NavLink>*/}
-
-                <Route  path="/tictactoe" exact ={true} component={TicTacToeGame}/>
-                {/*<NavLink strict exact to="/tictactoe">TicTacToe Game</NavLink>*/}
-
-            </div>
-        </BrowserRouter>
+        </div>
     );
 }
 
