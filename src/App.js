97f1ea6108d033e4f4d23b572aadaecbc066a64f
SebNik <lord.voldmeort1492@gmail.com>
2021-04-06 09:30:02 +0200

    restructuring all files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import Game from './components/Game'
 import './App.css';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, NavLink} from "react-router-dom";
 import Route from "react-router-dom/Route"
 
 function App() {
     return (
         <BrowserRouter>
             <div className="layout">
-                <Route  path="/" render={
+                <Route  path="/" exact ={true} render={
                     () => {
                         return <h1>Niklas Test</h1>
                     }
                 }/>
 
-                <Route  path="/" render={
+                <Route  path="/about" exact ={true} render={
                     () => {
-                        return <h1>Niklas Test</h1>
+                        return <h1>Niklas Test 2</h1>
                     }
                 }/>
+
+                <Route  path="/ticktacktoe" exact ={true} component={Game}/>
 
             </div>
         </BrowserRouter>
