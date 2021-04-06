429a547b454d9b9990379ac369f2749a8bf15e12
SebNik <lord.voldmeort1492@gmail.com>
2021-04-06 16:43:34 +0200

    cleaned up build better structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TicTacToeGame from './games/tic_tac_toe/Game'
+import About from "./pages/About";
 import './App.css';
 import {BrowserRouter, NavLink} from "react-router-dom";
 import Route from "react-router-dom/Route"
@@ -8,17 +9,9 @@ function App() {
     return (
         <BrowserRouter>
             <div className="layout">
-                <Route  path="/" exact ={true} render={
-                    () => {
-                        return <h1>Niklas Test</h1>
-                    }
-                }/>
 
-                <Route  path="/about" exact ={true} render={
-                    () => {
-                        return <h1>Niklas Test 2</h1>
-                    }
-                }/>
+                <Route  path="/about" exact ={true} component={About}/>
+                <NavLink strict exact to="/about">About</NavLink>
 
                 <Route  path="/tictactoe" exact ={true} component={TicTacToeGame}/>
                 <NavLink strict exact to="/tictactoe">TicTacToe Game</NavLink>
---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+// this is hte about page providing information about the owners and goals
+import React from 'react';
+
+function About() {
+    return (
+        <p>About Page</p>
+    )
+}
+
+export default About;
