40f85066552007c19dc875b4fc2f70d73c86b302
SebNik <lord.voldmeort1492@gmail.com>
2021-04-07 08:51:43 +0200

    first basic routing works or does the job

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
 import Navigation from "./pages/Navigation";
+import {BrowserRouter as Router,Switch, Route, Redirect} from "react-router-dom";
 
 import './assets/scss/base.scss'
 
 function App() {
     return (
-        <div className="App">
-            <Navigation/>
-        </div>
+        <Router>
+            <div className="App">
+                <Navigation/>
+            </div>
+        </Router>
     );
 }
 
