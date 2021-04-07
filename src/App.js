24f7a17911c8a6d121196a65234a160eaebee805
SebNik <lord.voldmeort1492@gmail.com>
2021-04-07 09:21:52 +0200

    working routing and all arround that

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import './App.css';
 import Navigation from "./pages/Navigation";
 import {BrowserRouter as Router,Switch, Route, Redirect} from "react-router-dom";
-
+import PageRenderer from "./page-renderer";
 import './assets/scss/base.scss'
+
 
 function App() {
     return (
         <Router>
             <div className="App">
                 <Navigation/>
+                <Switch>
+                    <Route path="/:page" component={PageRenderer}/>
+                    <Route path="/" render={() => <Redirect to="/home" />} />
+                    <Route component={() => 404} />
+                </Switch>
             </div>
         </Router>
     );
