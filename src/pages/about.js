8ca9a3587bb2101bc8efa430cb1cec17f706f2fa
SebNik <lord.voldmeort1492@gmail.com>
2021-04-07 22:54:36 +0200

    changed naming of all the files

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,6 @@
 // this is hte about page providing information about the owners and goals
 import React from 'react';
 
-function about() {
-    return (
-        <p>About Page</p>
-    )
+export default function about () {
+    return <div>About Page</div>
 }
-
-export default about;
