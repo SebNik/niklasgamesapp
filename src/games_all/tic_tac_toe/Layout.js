c786577a63362f8a8e7fbbc364c312e7d3b9de01
SebNik <lord.voldmeort1492@gmail.com>
2021-04-16 16:47:03 +0200

    now scss files for the tictactoe game

--- src/games_all/tic_tac_toe/Layout.js
+++ src/games_all/tic_tac_toe/Layout.js
@@ -3,7 +3,7 @@ import Box from './Box'
 
 function Layout({boxes, onClick}) {
     return (
-        <div style={style}>
+        <div className="layout-tictactoe">
             {boxes.map((box, i) => (
                 <Box key={i} value={box} onClick={() => onClick(i)}/>
             ))}
@@ -11,14 +11,14 @@ function Layout({boxes, onClick}) {
     );
 }
 
-const style = {
-    border: '4px solid lightblue',
-    borderRadius: '10px',
-    width: '320px',
-    height: '320px',
-    margin: '0 auto',
-    display: 'grid',
-    gridTemplate: 'repeat(3, 1fr) / repeat(3, 1fr)'
-};
+// const style = {
+//     border: '4px solid lightblue',
+//     borderRadius: '10px',
+//     width: '20%',
+//     height: cal(20% * 100vw),
+//     margin: '0 auto',
+//     display: 'grid',
+//     gridTemplate: 'repeat(3, 1fr) / repeat(3, 1fr)'
+// };
 
 export default Layout;
