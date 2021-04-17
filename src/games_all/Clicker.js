f622a5674c34a0d25908e3f044c7f8a0733498c2
SebNik <lord.voldmeort1492@gmail.com>
2021-04-17 12:08:06 +0200

    working Clicker.js

--- src/games_all/Clicker.js
+++ src/games_all/Clicker.js
@@ -1,15 +1,24 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 export default function Clicker() {
     // Declare a new state variable, which we'll call "count"
     const [count, setCount] = useState(0);
 
+    const clickHandler = ({keyCode}) => {
+        if (keyCode === 37) {
+            console.log("left-minus")
+            setCount(count - 1)
+        } else if (keyCode === 39) {
+            console.log("right-plus")
+            setCount(count + 1)
+        }
+    }
+
     return (
-        <div>
+        <div className={"clicker-design"} onKeyDown={clickHandler} tabIndex="0">
             <p>You clicked {count} times</p>
-            <button onClick={() => setCount(count + 1)}>
-                Click me
-            </button>
+            <button className={"button-clicker-plus"} onClick={() => setCount(count + 1)}>+</button>
+            <button className={"button-clicker-minus"} onClick={() => setCount(count - 1)}>-</button>
         </div>
     );
 }
