24368ed74bab589fb2701702e6232c9e8adcf7a7
SebNik <lord.voldmeort1492@gmail.com>
2021-04-19 17:12:18 +0200

    first working store

--- src/games_all/flappybird.js
+++ src/games_all/flappybird.js
@@ -7,6 +7,8 @@ import store from "./flappybird/store/flappybird_store.js"
 export default function FlappyBird() {
 
     console.log('Initial state: ', store.getState())
+    store.dispatch({ type: 'bird/falling' })
+
 
     return (
         // <Provider store={store}>
--- src/games_all/flappybird/store/reducer.js
+++ src/games_all/flappybird/store/reducer.js
@@ -6,27 +6,25 @@ const initialState = {
     },
     bird: {
         status: 'normal',
-            height: 188,
-            startHeight: 188,
+        height: 188,
+        startHeight: 188,
     }
 }
 
 export default function reducer(state = initialState, action) {
     switch (action.type) {
         case 'bird/falling': {
-            // Can return just the new todos array - no extra object around it
-            return [
+            return {
                 ...state,
-                {
-                    bird: {
-                        status: 'falling',
+                bird: {
+                    status: 'falling',
                         height: state.bird.height - 20,
                         startHeight: 188,
-                    }
                 }
-            ]
+            }
+
         }
         default:
-            break;
+            return state;
     }
 }
