bda7b67ccac3d1c70149e05f9b1c89016e7f9b3c
SebNik <lord.voldmeort1492@gmail.com>
2021-04-24 19:40:33 +0200

    drawing all the possible pipes

--- src/games_all/flappybird/piping.js
+++ src/games_all/flappybird/piping.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import Pipe from "./pipe";
+import {useSelector} from "react-redux";
+import {Link} from "react-router-dom";
+
+const selectPipeData = state => state.piping
 
 
 export default function Piping() {
+
+    const data = useSelector(selectPipeData)
+
+    function DrawPipes(data) {
+        for (let i = 0; i < data.heights.length; i++) {
+            <Pipe i={i}/>
+        }
+    }
+
     return (
         <div className={"piping-flappy-bird"}>
-            <Pipe i={0}/>
+            {/*<Pipe i={0}/>*/}
+            {/*<DrawPipes data={data}/>*/}
+            {data.heights.map((d, index) => (
+                <Pipe i={index} />
+            ))}
         </div>
     )
 }
