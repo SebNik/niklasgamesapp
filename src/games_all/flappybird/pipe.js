85499e642050f80cedcb6e5b3eff44ba907fca2d
SebNik <lord.voldmeort1492@gmail.com>
2021-04-24 19:31:28 +0200

    basics now woking with a single pipe

--- src/games_all/flappybird/pipe.js
+++ src/games_all/flappybird/pipe.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {useSelector, useDispatch} from "react-redux";
 
 const selectPipeXOffset = state => state.piping.x_offset
-const selectPipeYOffset = state => state.piping.y_offset
+// const selectPipeYOffset = state => state.piping.y_offset
 const selectPipeHeights = state => state.piping.heights
 const selectPipeSpace = state => state.piping.height_space
 
@@ -10,13 +10,13 @@ const selectPipeSpace = state => state.piping.height_space
 export default function Pipe(props) {
 
     const x_offset = useSelector(selectPipeXOffset)[props.i]
-    const y_offset = useSelector(selectPipeYOffset)[props.i]
+    // const y_offset = useSelector(selectPipeYOffset)[props.i]
     const height = useSelector(selectPipeHeights)[props.i]
     const space = useSelector(selectPipeSpace)
 
 
     let style_upper = {
-        transform: `translate(${x_offset}px, ${y_offset}px)`,
+        transform: `translate(${x_offset}px, 0px)`,
         height: `${height}px`
     }
 
--- src/games_all/flappybird/store/initialState.js
+++ src/games_all/flappybird/store/initialState.js
@@ -5,10 +5,10 @@ export default {
         interval_id: null,
     },
     bird: {
-        // gravitation: 1.5,
-        // fly_up_speed: -16,
-        gravitation: 0.00001,
-        fly_up_speed: 0.00001,
+        gravitation: 1.5,
+        fly_up_speed: -13,
+        // gravitation: 0.00001,
+        // fly_up_speed: 0.00001,
         status: 'normal',
         height: 0,
         startHeight: 188,
@@ -16,9 +16,8 @@ export default {
     },
     piping: {
         space_between_pipes: 200,
-        height_space: 60,
-        x_offset: [1600, 100],
-        y_offset: [0, 0],
+        height_space: 120,
+        x_offset: [1600],
         heights: [300],
         scroll_speed: -1.5,
     }
