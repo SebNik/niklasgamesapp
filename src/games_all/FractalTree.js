fc39751e8a3d955d75fc937edf4325cfc622b705
SebNik <lord.voldmeort1492@gmail.com>
2021-05-04 17:43:47 +0200

    working a simple tree for fun

--- src/games_all/FractalTree.js
+++ src/games_all/FractalTree.js
@@ -1,27 +1,55 @@
 import React, { useState } from 'react'
 import Canvas from "./FractalTree/canvas";
 import Slider from '@material-ui/core/Slider';
+import Grid from '@material-ui/core/Grid';
 
 export default function FractalTree() {
 
+    const [depth, setDepth] = useState(1);
+    const [length, setLength] = useState(1);
+    const [fraction, setFraction] = useState(1);
+    const [angle, setAngle] = useState(1);
 
-    const [value, setValue] = useState(30);
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
-
-    const draw = (ctx, test) => {
+    const draw = (ctx, data) => {
+        var deg_to_rad = Math.PI / 180.0;
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
         ctx.beginPath()
-        ctx.moveTo(0, 0);
-        ctx.lineTo(150, test.test);
+
+
+        function drawLine(x1, y1, x2, y2, brightness){
+            ctx.moveTo(x1, y1);
+            ctx.lineTo(x2, y2);
+        }
+
+        function drawTree(x1, y1, angle, depth, len, f, a){
+            if (depth !== 0){
+                var x2 = x1 + (Math.cos(angle * deg_to_rad)  * len);
+                var y2 = y1 + (Math.sin(angle * deg_to_rad)  * len);
+                drawLine(x1, y1, x2, y2, depth);
+                drawTree(x2, y2, angle - a, depth - 1, len * f, f, a);
+                drawTree(x2, y2, angle + a, depth - 1, len * f, f, a);
+            }
+        }
+
+        drawTree(250,500, -90, data.depth, data.start_length, data.fraction, data.angle)
+
+        ctx.closePath()
         ctx.stroke();
     }
     return (
         <div className={"fractal_tree_screen"}>
-            <Canvas draw={draw} settings={{test: value}} height={"500px"} width={"500px"} test={200}/>
-            <Slider value={value} onChange={handleChange} aria-labelledby="continuous-slider" />
+            <Canvas draw={draw} settings={{start_length: length, depth: depth, fraction: fraction, angle: angle}} height={"500px"} width={"500px"}/>
+            <div className={"fractal_tree_settings"}>
+                <p>Depth</p>
+                <Slider value={depth} onChange={(event, newDepth) => {setDepth(newDepth)}} min={0} max={20}/>
+                <p>Start length</p>
+                <Slider value={length} onChange={(event, newLength) => {setLength(newLength)}} min={10} max={100}/>
+                <p>Length fraction</p>
+                <Slider value={fraction*100} onChange={(event, newValue) => {setFraction(newValue/100)}} min={0} max={100}/>
+                <p>Angle</p>
+                <Slider value={angle} onChange={(event, newValue) => {setAngle(newValue)}} min={-90} max={90}/>
+            </div>
         </div>
     )
 }
