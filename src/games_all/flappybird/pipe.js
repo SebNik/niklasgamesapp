00f40c70b96fdd3e3d5b0e2e719b69d5cec7b5a7
SebNik <lord.voldmeort1492@gmail.com>
2021-05-04 10:51:10 +0200

    cleaned up the old unwanted stuff

--- src/games_all/flappybird/pipe.js
+++ src/games_all/flappybird/pipe.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import {useSelector, useDispatch} from "react-redux";
+import {useSelector} from "react-redux";
 
 const selectPipeXOffset = state => state.piping.x_offset
-// const selectPipeYOffset = state => state.piping.y_offset
 const selectPipeHeights = state => state.piping.heights
 const selectPipeSpace = state => state.piping.height_space
 
@@ -10,7 +9,6 @@ const selectPipeSpace = state => state.piping.height_space
 export default function Pipe(props) {
 
     const x_offset = useSelector(selectPipeXOffset)[props.i]
-    // const y_offset = useSelector(selectPipeYOffset)[props.i]
     const height = useSelector(selectPipeHeights)[props.i]
     const space = useSelector(selectPipeSpace)
 
