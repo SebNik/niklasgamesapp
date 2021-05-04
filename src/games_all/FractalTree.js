9269ca8953053e60bba43e1391100f86590fbd05
SebNik <lord.voldmeort1492@gmail.com>
2021-05-04 16:49:41 +0200

    wokring slider with very very simple canvas

--- src/games_all/FractalTree.js
+++ src/games_all/FractalTree.js
@@ -1,8 +1,15 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useState } from 'react'
 import Canvas from "./FractalTree/canvas";
-
+import Slider from '@material-ui/core/Slider';
 
 export default function FractalTree() {
+
+
+    const [value, setValue] = useState(30);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
     const draw = (ctx, test) => {
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
@@ -10,12 +17,11 @@ export default function FractalTree() {
         ctx.moveTo(0, 0);
         ctx.lineTo(150, test.test);
         ctx.stroke();
-        ctx.fill()
     }
     return (
         <div className={"fractal_tree_screen"}>
-            <Canvas draw={draw} settings={{test: 500}
-            } height={"500px"} width={"500px"} test={200}/>
+            <Canvas draw={draw} settings={{test: value}} height={"500px"} width={"500px"} test={200}/>
+            <Slider value={value} onChange={handleChange} aria-labelledby="continuous-slider" />
         </div>
     )
 }
