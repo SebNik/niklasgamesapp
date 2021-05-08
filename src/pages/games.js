4c7af498ad68c3d60dc01ce6e9f2de5c07f4d869
SebNik <lord.voldmeort1492@gmail.com>
2021-05-08 12:40:30 +0200

    refactored code

--- src/pages/games.js
+++ src/pages/games.js
@@ -8,15 +8,14 @@ import Background from '../assets/images/game.png';
 export default function Games() {
 
     function position(index) {
-        let column_start = index - (Math.trunc(index/4)*4)
-        let row_start = (Math.trunc(index/4))
-        let style = {
-            gridRow: `${row_start+2} / ${row_start+3}`,
-            gridColumn: `${column_start+2} / ${column_start+3}`,
+        let column_start = index - (Math.trunc(index / 4) * 4)
+        let row_start = (Math.trunc(index / 4))
+        return {
+            gridRow: `${row_start + 2} / ${row_start + 3}`,
+            gridColumn: `${column_start + 2} / ${column_start + 3}`,
             // TODO ADD the background image here
             backgroundImage: `url(${Background})`,
-    }
-        return style
+        }
     }
 
     return (
