fd67d5a5331668e2de59af177b6fb3f874219d6f
SebNik <lord.voldmeort1492@gmail.com>
2021-05-08 12:39:06 +0200

    adding new grid and an background image

--- src/pages/games.js
+++ src/pages/games.js
@@ -3,16 +3,19 @@ import React from "react";
 import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
 import {navLinks} from "./navLinksGames";
 
+import Background from '../assets/images/game.png';
+
 export default function Games() {
 
     function position(index) {
-        let column_start = index - (Math.trunc(index/5)*5)
-        let row_start = (Math.trunc(index/5))
+        let column_start = index - (Math.trunc(index/4)*4)
+        let row_start = (Math.trunc(index/4))
         let style = {
             gridRow: `${row_start+2} / ${row_start+3}`,
             gridColumn: `${column_start+2} / ${column_start+3}`,
             // TODO ADD the background image here
-        }
+            backgroundImage: `url(${Background})`,
+    }
         return style
     }
 
