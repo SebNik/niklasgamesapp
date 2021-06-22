c92ab285ac38d8d23dfe83c235e16a08e14d8592
SebNik <lord.voldmeort1492@gmail.com>
2021-06-22 19:41:49 +0200

    added dropdown function for settings

--- src/component/DropDownMenu/DropDownSingle.js
+++ src/component/DropDownMenu/DropDownSingle.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import FontAwesome from 'react-fontawesome';
 
 class DropDownSingle extends Component {
     
@@ -10,11 +11,70 @@ class DropDownSingle extends Component {
         }
     }
 
+    static getDerivedStateFromProps(nextProps) {
+        const { list, title } = nextProps;
+        const selectedItem = list.filter((item) => item.selected);
+      
+        if (selectedItem.length) {
+          return {
+            headerTitle: selectedItem[0].title,
+          };
+        }
+        return { headerTitle: title };
+    }
+
+    toggleList = () => {
+        this.setState(prevState => ({
+            isListOpen: !prevState.isListOpen
+        }))
+    }
+
+    selectItem = (item) => {
+        const { resetThenSet } = this.props;
+        const { title, id, key } = item;
+
+        this.setState({
+            headerTitle: title,
+            isListOpen: false,
+        }, () => resetThenSet(id, key));
+    }
+
     render () {
         const { isListOpen, headerTitle } = this.state;
+        const { list } = this.props;
 
         return (
-            <p>{headerTitle}</p>
+            <div className="dd-wrapper">
+            <button
+                type="button"
+                className="dd-header"
+                onClick={this.toggleList}
+            >
+                <div className="dd-header-title">{headerTitle}</div>
+                {isListOpen
+                ? <FontAwesome name="angle-up" size="2x" />
+                : <FontAwesome name="angle-down" size="2x" />}
+            </button>
+            {isListOpen && (
+                <div
+                role="list"
+                className="dd-list"
+                >
+                {list.map((item) => (
+                    <button
+                    type="button"
+                    className="dd-list-item"
+                    key={item.id}
+                    onClick={() => this.selectItem(item)}
+                    >
+                    {item.title}
+                    {' '}
+                    {item.selected && <FontAwesome name="check" />}
+                    </button>
+                ))}
+                </div>
+            )}
+            </div>
         )
     }
 }
--- src/games_all/polar_coordinates/menu.js
+++ src/games_all/polar_coordinates/menu.js
@@ -1,28 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 import DropDownSingle from '../../component/DropDownMenu/DropDownSingle'
 import { options_polar_coordinates } from './options'
 
-const Menu = props => {
+class Menu extends Component {
 
+    resetThenSet = (id, key) => {
+        const temp = [...this.state[key]];
+      
+        temp.forEach((item) => item.selected = false);
+        temp[id].selected = true;
+      
+        this.setState({
+          [key]: temp,
+        });
+    }
+
+    render () {
+        return (
+            <div className={"polar-coordinates-settings"}>
+                {/* INFO */}
+                {/* This is he first dropdown menu to set the general working enviroment we are currently in. */}
+                <DropDownSingle
+                    headerTitle="Select simulation"
+                    list={options_polar_coordinates}
+                    resetThenSet={this.resetThenSet}
+                ></DropDownSingle>
     
-    return (
-        <div className={"polar-coordinates-settings"}>
-            {/* INFO */}
-            {/* This is he first dropdown menu to set the general working enviroment we are currently in. */}
-            <DropDownSingle
-                headerTitle="Select simulation"
-                list={options_polar_coordinates}
-            ></DropDownSingle>
-
-            {/* Here are all the settings. With the variable number of input options, defined by the dropdown fields. */}
-            <form>
-                <label for="fname">First input:</label><br/>
-                <input type="range" className="slider" name="mySlider" id="newspeed" min={0} max={100} value={50} /><br/>
-                <label for="fname">First name:</label><br/>
-                <input type="text" id="fname" name="fname"/><br/>
-            </form>
-        </div>
-    )
+                {/* Here are all the settings. With the variable number of input options, defined by the dropdown fields. */}
+                <form>
+                    <label for="fname">First input:</label><br/>
+                    <input type="range" className="slider" name="mySlider" id="newspeed" min={0} max={100} value={50} /><br/>
+                    <label for="fname">First name:</label><br/>
+                    <input type="text" id="fname" name="fname"/><br/>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default Menu
